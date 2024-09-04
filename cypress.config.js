961974f63dcfd453c2bb6a0dd38a0fbf90a0b364
NivaldoBertolazoJunior <nivaldo_bj@hotmail.com>
2024-09-04 16:27:15 -0300

    some alterations on config

--- cypress.config.js
+++ cypress.config.js
@@ -13,7 +13,6 @@ module.exports = defineConfig({
     mode: "test",
     screenshotFolder: 'screenshots',
     videoFolder: 'videos',
-    apiToken: '96306c0af74a2959c9302569387b493c8a70d39375b0fe18b7b4e03bde1924bb',
     projectCode: 'TEST',
     logging: true,
     basePath: 'https://api.qase.io/v1',
